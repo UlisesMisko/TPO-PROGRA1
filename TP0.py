e728a31b9a05b9e92b1875f0ead6150cba441e9b
UlisesMisko <150976979+UlisesMisko@users.noreply.github.com>
2023-11-16 11:01:57 -0300

    Update TP0.py

--- TP0.py
+++ TP0.py
@@ -19,8 +19,8 @@ def cant_goles(archivo):      #Muestra los 3 equipos con mayores goles
 
 
 def dif_gol(archivo, equipos_lst):     #Mostrar las diferencias de goles DE TODAS las fechas para aquellos 3 equipos que tengan mas goles en todas las fechas FIFA.
-    resultados_por_equipo = {equipo: {} for equipo in equipos_lst}
-
+    resultados_por_equipo = {equipo: {} for equipo in equipos_lst}        """Uso diccionario por comprension y ademas agrego otro diccionario dentro para manejar de manera mas eficiente los valores como [fecha].
+                                                                             Esto fue uno de los temas que mas tuve que investigar por mi propia cuenta. Solicito si es posible una aclaracion.""" 
     for columnas in archivo:
         fecha, eq_loc, eq_vis, eq_marcador = columnas[:4]       
              
@@ -55,8 +55,8 @@ def max_dif(resultados_por_equipo):   #Detectar el equipo (de los 3) con la maxi
       
       
 def lectura():   #Se encarga de abrir el archivo, si es que existe.
-    lista = []
-    try:
+    lista = []                                                      """Guardo todo en memoria ---> MAL"""                                                                      
+    try:                                                               #Arreglarlo
         x = input("Ingrese nombre del archivo")
         archivo = open(x,'r',encoding='utf-8')
         
@@ -70,7 +70,7 @@ def lectura():   #Se encarga de abrir el archivo, si es que existe.
             archivo.close()    
         
 
-def validacion():  #Acceso al programa
+def validacion():  #Acceso al programa            """Esta funcion la entendi como el ultimo enunciado del TPO en donde con otro grupo nos creamos la misma funcion con los mismos datos para acceder al archivo del cliente"""
     usuario = "InnovaCode"
     contraseña = "CodingCorporation"
     bandera = False
